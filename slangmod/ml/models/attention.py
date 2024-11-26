95dc555181baae552ff0c009089c26156c76d215
yedivanseven <georg@muckisnspirit.com>
2024-11-26 15:47:27 +0100

    Adapted attention to positional encodings

--- slangmod/ml/models/attention.py
+++ slangmod/ml/models/attention.py
@@ -3,7 +3,8 @@ from typing import Self
 import torch as pt
 import torch.nn as ptn
 import torch.nn.functional as ptnf
-from swak.pt.types import Device, Dtype, Tensor
+from swak.pt.types import Device, Dtype, Tensor, Module
+from swak.pt.misc import Identity
 from swak.pt.blocks import Block
 from ...config import config, LiteralDevice
 
@@ -21,6 +22,8 @@ class Attention(Block):
             n_heads: int,
             bias: bool = True,
             dropout: float = 0.1,
+            src_pos_enc: Module = Identity(),
+            qk_pos_enc: Module = Identity(),
             device: Device | LiteralDevice = 'cpu',
             dtype: Dtype = pt.float
     ) -> None:
@@ -29,6 +32,8 @@ class Attention(Block):
         self.n_heads = n_heads
         self.bias = bias
         self.dropout = dropout
+        self.src_pos_enc = src_pos_enc
+        self.qk_pos_enc = qk_pos_enc
         self.device = pt.device(device)
         self.dtype = dtype
         self.qkv = ptn.Linear(
@@ -64,15 +69,15 @@ class Attention(Block):
             mask: Tensor | None = None,
             is_causal: bool=True
     ) -> Tensor:
-        query, key, value = self.qkv(src).chunk(3, -1)
+        query, key, value = self.qkv(self.src_pos_enc(src)).chunk(3, -1)
 
         query = query.unflatten(-1, self.sizes).transpose(-2, -3)
         key = key.unflatten(-1, self.sizes).transpose(-2, -3)
         value = value.unflatten(-1, self.sizes).transpose(-2, -3)
 
         attended = ptnf.scaled_dot_product_attention(
-            query=query,
-            key=key,
+            query=self.qk_pos_enc(query),
+            key=self.qk_pos_enc(key),
             value=value,
             attn_mask=None if is_causal else mask,
             dropout_p=self.dropout if self.training else 0.0,
@@ -92,6 +97,8 @@ class Attention(Block):
             self.n_heads,
             self.bias,
             self.dropout,
+            self.src_pos_enc,
+            self.qk_pos_enc,
             self.device,
             self.dtype
         )
